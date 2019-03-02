559b45feccdc9adccda1c4992e2a23a5cd03dcdf
Colinguner <47030477+Colinguner@users.noreply.github.com>
2019-03-03 00:16:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 // _______________________Variables_________________
 var fruitOnScreen = false;
-// var gameStarted = false;
 var gameLost = false;
-var direction = "right";
-var leng = 4;
+var direction = "right"; 
 var snake = [];
 var fruit = undefined;
-var ate = false;
+var ate = false; //if fruit was eaten
 let score = document.querySelector(".score");
 var w = screen.width;
 var ctx = document.getElementById('canvas').getContext("2d");
 var gamePaused = false;
-var playingGame;
+var playingGame; //itervalID
 var loseSound = new Audio("sounds/lose.wav");
 let eat = new Audio("sounds/eat.wav");
+let pik = new Audio("sounds/pik.wav");
+// ___________________________________
+const arrows = document.querySelectorAll(".fas");
+const upKey = document.getElementById("up");
+const downKey = document.getElementById("down");
+const leftKey = document.getElementById("left");
+const rightKey = document.getElementById("right");
+const pauseButton = document.getElementById("pause");
+const playButton = document.getElementById("play");
 // _______________________Variables_________________
-
 //crating starting snake
-for (let i = leng - 1; i >= 0; i--) {
+for (let i = 3; i >= 0; i--) {
   snake.push({
     x: i,
     y: 5
@@ -25,27 +31,13 @@ for (let i = leng - 1; i >= 0; i--) {
 }
 
 if (w <= 700) {
-  // Arrows and buttons DOM
-  let arrows = document.querySelectorAll(".fas");
-  const upKey = document.getElementById("up");
-  const downKey = document.getElementById("down");
-  const leftKey = document.getElementById("left");
-  const rightKey = document.getElementById("right");
-  const pauseButton = document.getElementById("pause");
-  const playButton = document.getElementById("play");
-
-
   // __________Changing layout_________________________
   document.getElementById("canvas").setAttribute("height", "400");
   document.getElementById("canvas").setAttribute("width", "300");
   document.getElementById("smallScreenScore").style.display = 'block';
-  // __________Changing layout_________________________
-  // cvsW = canvas.width;
-  // cvsH = canvas.height;
   //___________EventListeners Arrows_______________________
   for (let i = 0; i < 6; i++) {
     arrows[i].addEventListener("click", function () {
-      let pik = new Audio("sounds/pik.wav");
       pik.play();
     });
   }
@@ -61,7 +53,7 @@ if (w <= 700) {
   rightKey.addEventListener("click", function () {
     direction = "right";
   });
-  //___________EventListeners Arrows_______________________
+  //___________Play/Pause_______________________
   pauseButton.addEventListener("click", function () {
     console.log("pauza");
     document.getElementById("pause").style.display = 'none';
@@ -78,8 +70,6 @@ if (w <= 700) {
     console.log(gamePaused);
     playingGame = setInterval(playing, 160);
   });
-
-
   playingGame = setInterval(playing, 160);
   drawFruit();
 } else {
@@ -91,10 +81,7 @@ if (w <= 700) {
   // cvsH = canvas.height;
   drawFruit();
 }
-
-
 // ________________________FUNCTIONS_______________________________
-
 function playing() {
   //checking if snake hit wall
   hitWall();
@@ -134,8 +121,7 @@ function playing() {
     x: snake[snake.length - 1].x,
     y: snake[snake.length - 1].y,
   }
-  // _________________________________________________
-  //chaning path behind snake back to black
+  //chaning path behind snake back to color black
   ctx.fillStyle = "black";
   ctx.fillRect(tail.x * 10, tail.y * 10, 10, 10);
   // _____________________fruit_______________
@@ -143,7 +129,6 @@ function playing() {
     ate = true;
   }
   if (ate === false) {
-    //
     snake.pop();
   }
   if (ate === true) {
@@ -154,6 +139,7 @@ function playing() {
     ate = false;
   }
   // _____________________fruit_______________
+  //drawing everything
   draw();
 }
 //______________________DRAWING________________
@@ -181,7 +167,6 @@ function drawFruit() {
   };
   ctx.fillStyle = "red";
   ctx.fillRect(randomX, randomY, 10, 10);
-  console.log(fruit);
 }
 
 function scoreChange() {
@@ -192,7 +177,6 @@ function scoreChange() {
 }
 //______________________DRAWING________________
 
-
 function gameLostFunction() {
   //red screen flash on lose
   ctx.fillStyle = "red";
@@ -201,11 +185,10 @@ function gameLostFunction() {
   setTimeout(function () {
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    drawFruit();
-  }, 80);
+    drawFruit();}, 80);
   //restarting game
   snake = [];
-  for (let i = leng - 1; i >= 0; i--) {
+  for (let i = 3; i >= 0; i--) {
     snake.push({
       x: i,
       y: 5
@@ -219,11 +202,9 @@ function gameLostFunction() {
 function hitWall() {
   if (snake[0].x === (canvas.width / 10) || snake[0].x === -1) {
     gameLost = true;
-    //loseSoundFunc();
     loseSound.play();
   } else if (snake[0].y === (canvas.height / 10) || snake[0].y === -1) {
     gameLost = true;
-    //loseSoundFunc();
     loseSound.play();
   }
 }
@@ -232,18 +213,7 @@ function snakeCollisionWithTailCheck() {
   for (let i = 1; i < snake.length; i++) {
     if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) {
       gameLost = true;
-      //loseSoundFunc();
       loseSound.play();
     }
   }
 }
-//SOUNDS
-// function loseSoundFunc() {
-//   var loseSound = new Audio("sounds/lose.wav");
-//   loseSound.play();
-// }
-
-// function eatSound() {
-//   let audio = new Audio("sounds/eat.wav");
-//   audio.play();
-// }
